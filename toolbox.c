cf85bf9b4b0d801554473ddf7521ca642df16a8e
Nanda Gururajan <ngururajan@umass.edu>
2026-02-15 19:24:25 -0500

    basic sleep added

--- toolbox.c
+++ toolbox.c
@@ -1,24 +1,46 @@
 #include <stdio.h>
 #include <math.h>
+#include "esp_sleep.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 #define PRESENT 1
 #define CONFLICT 2
 #define UNKNOWN 3
 #define ABSENT 4
 
-unsigned long tool_rfid = 2847104201;     // Random RFID Tag
-float tool_weight = 250;                  // Random Tool Weight (grams)
 #define TOLERANCE 5.0
 
-int toolbox_check(int rfid, float weight) {
-    if (rfid == tool_rfid && fabs(weight - tool_weight) < TOLERANCE){
+unsigned long tool_rfid = 2847104201UL;
+float tool_weight = 250.0;
+
+int toolbox_check(unsigned long rfid, float weight) {
+
+    if (rfid == tool_rfid && fabs(weight - tool_weight) < TOLERANCE) {
         return PRESENT;
-    } else if (rfid == tool_rfid && fabs(weight - tool_weight) < TOLERANCE){
+    }
+    else if (rfid == tool_rfid && fabs(weight - tool_weight) >= TOLERANCE) {
         return CONFLICT;
-    } else if (rfid != tool_rfid && fabs(weight - tool_weight) < TOLERANCE){
+    }
+    else if (rfid != tool_rfid && fabs(weight - tool_weight) < TOLERANCE) {
         return UNKNOWN;
-    } else {
+    }
+    else {
         return ABSENT;
     }
 }
 
+void app_main(void) {
+
+    unsigned long read_rfid = 2847104201UL;
+    float read_weight = 250.0;
+
+    int status = toolbox_check(read_rfid, read_weight);
+
+    printf("Tool status = %d\n", status);
+
+    printf("Going to sleep...\n");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    esp_deep_sleep_start();
+}
